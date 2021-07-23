411e051efa45fbdae066ec4e4e9d15beed032a48
Gokul <gokulprathin8@gmail.com>
2021-07-24 01:17:49 +0530

    init project

---
+++ keystone.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({
+
+  }),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
